eca6278afb32a956e530fa6cdb3d5289328030ce
Tara Roskell <tara@troskell.demon.co.uk>
2013-03-08 16:15:51 +0000

    PINTERESTED UPLOADED

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinterested::Application.config.secret_token = '07faf535732c360be10fc099dcfeae7b66dba930d5f7f04bfcd9f300a7bf90e128f71e7b7defa7838a58ac9811c9e14236acda1323545eadd0c53231a3b548c1'
